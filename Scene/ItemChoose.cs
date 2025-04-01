314fe05c7caade1c8a17b241645efc6ac9a2c2e0
01stone01 <hagwhr2@gmail.com>
2025-04-01 16:20:43 +0900

    [feat] add scene(itemchoose)

---
+++ Scene/ItemChoose.cs
@@ -0,0 +1,46 @@
+﻿namespace MudGame;
+
+public class ItemChoose : Scene
+{
+    private string key;
+    private int keyInt;
+    public override void Render()
+    {
+        Console.WriteLine("방공호에 가져갈 아이템을 선택해주세요");
+    }
+
+    public override void Choice()
+    {
+        for (int i = 1; i <= Game.items.Length; i++)
+        {
+            Console.WriteLine($"{i}. {Game.items[i-1]}");
+        }
+    }
+
+    public override void Input()
+    {
+        key = Console.ReadLine();
+        int.TryParse(key, out keyInt);
+    }
+    
+    public override void Result()
+    {
+        int itemCount = 0;
+        for (int i = 0; Game.items.Length; i++)
+        {
+            if (keyInt - 1 == i)
+            {
+                Player.Inventory[itemCount++] = Game.items[i];
+                Game.items.RemoveAt[i];
+            }
+        }
+    }
+    
+    public override void Next()
+    {
+        if (Player.Inventory.Length = 4)
+        {
+            Game.CurSceneChange("Ax");
+        }
+    }
+}
---
+++ Scene/Scene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+
+namespace MudGame;
+
+public abstract class Scene
+{
+    protected ConsoleKey _key;
+    
+    public abstract void Render();
+    
+    public abstract void Choice();
+
+    public virtual void Input()
+    {
+        _key = Console.ReadKey(true).Key;
+    }
+    
+    public abstract void Result();
+
+    public void Wait()
+    {
+        Console.WriteLine("계속 진행하려면 아무 키나 누르세요.");
+        Console.ReadKey();
+    }
+    
+    public abstract void Next();
+}
