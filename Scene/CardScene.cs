450563d58c8eb6d40ae54c2eb2e091a50eb0678c
76sohyun <conut1119@gmail.com>
2025-04-01 21:37:38 +0900

    [feat] 카드씬, 구급상자씬

---
+++ Scene/CardScene.cs
@@ -0,0 +1,62 @@
+﻿namespace MudGame;
+
+public class CardScene : Scene
+{
+    private Player player;
+    public override void Render()
+    {
+        Console.WriteLine("\"코카카칵 팍\"");
+        Console.WriteLine("밖에서 누가 문을 부시며 들어옵니다.");
+        Console.WriteLine("어떤 깡패무리들이 들어옵니다.");
+        Console.WriteLine("당신이 말합니다.");
+        Console.WriteLine("\"뭐하시는거예요!!!!!!!\"");
+        Console.WriteLine("깡패 중 대장이 말합니다.");
+        Console.WriteLine("\"하... 내가 너무 심심해서 그런데 물건 다 걸고 카드게임할래?\"");
+        Console.WriteLine("당신은 어떻게 할 것입니까?");
+    }
+
+    public override void Choice()
+    {
+        Console.WriteLine("1. 저... 카드게임 못합니다... 나가주시면 안될까요?");
+        Console.WriteLine("2. 네 한 번 뜨시죠!!!");
+        Console.WriteLine("3. 맞짱뜬다.");
+    }
+
+    public override void Result()
+    {
+        switch (_key)
+        {
+            case ConsoleKey.D1:
+                Console.WriteLine("깡패 대장이 당황합니다.");
+                Console.WriteLine("\"너 왜이렇게 쫄보니? 하.... 그런 xx들이 젤 싫어 퉤\"");
+                Console.WriteLine("당신의 얼굴에 침을 뱉었습니다.");
+                Console.WriteLine("당신이 말합니다.");
+                Console.WriteLine("제발 살려주세요");
+                Console.WriteLine("대장은 당신의 얼굴에 죽빵을 날리고 부하들과 떠납니다.");
+                Console.WriteLine("당신의 HP가 1 깎였습니다.");
+                player.hp--;
+                Console.WriteLine("아프당.............");
+                break;
+
+            case ConsoleKey.D2:
+                Console.WriteLine("카드게임을 시작합니다...");
+                Console.WriteLine("2시간 게임 끝에 당신이 이겼습니다.");
+                Game.player.inventory.Remove("Card");
+                Console.WriteLine("\"야 너 잘한다?\"");
+                Console.WriteLine("하하......");
+                Console.WriteLine("\"내가 졌으니 총 한 자루 주마.\"");
+                Console.WriteLine("대장이 총을 준 후 깡패 부하들이 문을 고치고 떠납니다...(착하네요)");
+                break;
+
+            case ConsoleKey.D3:
+                Console.WriteLine("\"당신은 갑자기 분노조절장애가 생기며 대장에게 주먹을 날립니다.\"");
+                Console.WriteLine("대장의 부하들이 당신을 엄청나게 팹니다.... 퍽퍼퍼퍽");
+                break;
+        }
+    }
+
+    public override void Next()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Scene/FirstAidKitScene.cs
@@ -0,0 +1,55 @@
+﻿namespace MudGame;
+
+public class FirstAidKitScene : Scene
+{
+    Player player;
+    public override void Render()
+    {
+        Console.WriteLine("당신의 방공호의 전기가 끊긴지 3일이 지났습니다...");
+        Console.WriteLine("당신은 오들오들 떱니다....");
+        Console.WriteLine("아무래도 몸살에 걸린 것 같습니다.");
+        Console.WriteLine("약을 안먹으면 죽을 것 같습니다...");
+        Console.WriteLine("당신은 어떻게 할 것입니까?");
+    }
+
+    public override void Choice()
+    {
+        Console.WriteLine("1. 이불 덮고 잔다...");
+        Console.WriteLine("2.엇... 마침 옆집 아저씨가 의사인게 떠올랐다.. 집 밖으로 나가서 옆집으로 간다.");
+        Console.WriteLine("3. 구급상자 안에 있는 약을 먹는다.");
+    }
+
+    public override void Result()
+    {
+        switch (_key)
+        {
+            case ConsoleKey.D1:
+                Console.WriteLine("당신은 이불을 꽁꽁 싸매서 잔다....");
+                Console.WriteLine("하지만 보일러가 잘 안되는 덕에 감기몸살에 걸렸습니다..");
+                Console.WriteLine("당신은 일주일내내 앓았습니다..");
+                Console.WriteLine("오늘의 교훈 약을 아끼지말자.");
+                Console.WriteLine("당신의 HP가 2 깎였습니다.");
+                player.hp--;
+                player.hp--;
+                break;
+
+            case ConsoleKey.D2:
+                Console.WriteLine("당신은 옆 집에 가기 위해 얼굴을 손수건으로 가리고 나갑니다. ");
+                Console.WriteLine("하지만 손수건은 방사능을 이길 수 없었습니다...");
+                Console.WriteLine("\"커억어엉어어억\"");
+                Console.WriteLine("당신은.,.. 방사능에 의해 문도가 되었습니다.");
+                break;
+
+            case ConsoleKey.D3:
+                Console.WriteLine("\"띠리리릴리ㅣㄹ뾰보봅보보봅보\"");
+                Console.WriteLine("당신은 약을 먹고 하루 정도 쉬자 몸살이 나았습니다.");
+                Console.WriteLine("이거제~~~~~~~~");
+                break;
+        }
+    }
+
+    public override void Next()
+    {
+        throw new NotImplementedException();
+    }
+}
